2bb05e191b8d24533c713c4546e2b1d94da87174
zakifrhn <zaki.farhan026@gmail.com>
2023-10-06 14:32:19 +0700

    minggu ke-2

---
+++ minggu ke-2.js
@@ -0,0 +1,27 @@
+// buat printTriangle (5) dgn ouput seperti dibawah ini
+// 1
+// 12
+// 123
+// 1234
+// 12345
+
+const Coba =(n)=>{
+for(i=0; i < n; i++){
+    for(j=i+1; j < n+1; j++){
+        console.loog(i);
+        console.log(j);
+    }
+}
+}
+
+// Coba (5)
+
+const printSquare = (n) =>{
+    for(i=0; i < n; i++){
+        for(j=i+1; j < n; j++){
+            console.log("*")
+        }
+    }
+}
+
+//printSquare(5)
